d9499a9f48df884f8232d51cda8e3786b20bf723
obiradige <akinfurkanerdogan@gmail.com>
2021-03-12 18:47:19 +0300

    created user model

--- models/user_model.js
+++ models/user_model.js
@@ -3,5 +3,18 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const User = Schema({
-    
+    username : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+    }
 })
