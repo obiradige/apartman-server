ae9931eeaf014d80aae3517d125f14bc9b41777a
obiradige <akinfurkanerdogan@gmail.com>
2021-03-12 19:27:55 +0300

    define user route

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require("./db/db_connect")
 const express = require("express")
 const port = process.env.PORT || 3000
-
 const app = express()
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
 
-
+app.use(express.json());
+const userRoute = require("./router/users");
+app.use("/user", userRoute);
 
 
 app.listen(port,()=>{
